8e296728660891c455eb759c9425fc0ee295f1e2
Marques Webster <marques.q.webster@gmail.com>
2023-02-03 00:34:34 -0600

    initial custom member card setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import { Button, Container, Stack } from 'react-bootstrap';
 import CustomHeader from './components/CustomHeader';
+import CustomCard from './components/CustomCard';
 import './App.css';
 import './custom.scss';
 
 export default function App() {
+  const [members, setMembers] = useState([
+    {
+      id: nanoid(),
+      name: 'Marques Webster',
+      desc: 'Head of Household',
+      fruit: 'Pineapple',
+    },
+  ]);
+
+  const memberElements = members.map((member) => {
+    return <CustomCard key={member.id} {...member} />;
+  });
+
   return (
     <>
       <CustomHeader />
+      <Container className='main'>
+        <h1 className='m-0'>Your household</h1>
+        <p className='m-0'>
+          Welcome to the Marketplace! Review your household below:
+        </p>
+        <Stack className='custom-card__list' direction='horizontal' gap={3}>
+          {memberElements}
+        </Stack>
+        <Button style={{ width: '280px', height: '60px', fontSize: '1.6rem' }}>
+          Add new member
+        </Button>
+      </Container>
     </>
   );
 }
---
+++ src/components/CustomCard.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+// import CurrMemberModal from './CurrMemberModal';
+
+export default function CustomCard(props) {
+  const [showCurr, setShowCurr] = useState(false);
+  const handleCurrOpen = () => setShowCurr(true);
+  const handleCurrClose = () => setShowCurr(false);
+
+  return (
+    <>
+      <Card
+        className='p-3'
+        style={{ minWidth: '360px', height: '180px', borderWidth: '2px' }}
+        onClick={handleCurrOpen}
+      >
+        <Card.Body className='d-flex flex-column justify-content-around'>
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>
+            <strong>Description:</strong> {props.desc}
+          </Card.Text>
+          <Card.Text>
+            <strong>Favorite Fruit:</strong> {props.fruit}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      {/* <CurrMemberModal
+        show={showCurr}
+        hide={handleCurrClose}
+        {...props}
+        delete={() => props.delete(props.id)}
+      /> */}
+    </>
+  );
+}
